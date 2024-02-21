5c8a26b1dd86db9c88afca78f1590f54a8ccd5b4
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-02-21 17:59:40 +0200

    Add patient routes endpoints and validation middlewares

---
+++ server/src/routes/patientRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const patientController = require('../controllers/patientController');
+const { loginValidation, registerValidation } = require('../middlewares/userValidator');
+const loginLimiter = require('../middlewares/loginLimiter');
+
+router.post('/login', loginValidation, loginLimiter, patientController.login);
+router.post('/register', registerValidation, patientController.register);
+router.post('/logout', patientController.logout);
+
+module.exports = router;
