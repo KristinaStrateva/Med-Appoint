bff2ab4d02e64a304aa00058e94d6f603686f1f4
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-02-21 17:00:56 +0200

    Add Doctor model

---
+++ server/src/models/Doctor.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const doctorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    medSpeciality: {
+        type: String,
+    },
+    appointments: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'Appointment',
+        }
+    ]
+});
+
+const Doctor = mongoose.model('Doctor', doctorSchema);
+
+module.exports = Doctor;
