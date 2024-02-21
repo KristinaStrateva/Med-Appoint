7448f1eadcc0ae70a9183d26691b14531fd320a7
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-02-21 16:59:32 +0200

    Add MongoDB configuration and install mongoose

---
+++ server/src/config/dbConfigurator.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+
+const uri = process.env.MONGO_URI;
+
+async function dbConnect() {
+    try {
+        await mongoose.connect(uri);
+
+    } catch (err) {
+        console.error(`DB Error: ${err}`);
+        console.error(err.message);
+        process.exit(1);
+    }
+}
+
+module.exports = dbConnect;
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const expressConfigurator = require('./config/expressConfigurator');
+const dbConnect = require('./config/dbConfigurator');
+// const { logEvents } = require('./middlewares/logger');
 
 const PORT = process.env.PORT || 3500;
 
 const app = express();
 
+dbConnect(app);
 expressConfigurator(app);
 
+mongoose.connection.once('open', () => {
+    console.log('DB connected successfully!');
+    
+    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
+});
+
+mongoose.connection.on('error', err => {
+    console.log(err);
+
+    // logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log');
+});
