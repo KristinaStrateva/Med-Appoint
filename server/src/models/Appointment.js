b624195b99f2a5fb887492a81e7ff9cc7dbbdbda
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-02-21 17:04:32 +0200

    Add Appointment model

---
+++ server/src/models/Appointment.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const appointmentSchema = new mongoose.Schema(
+    {
+        doctorId: {
+            type: mongoose.Types.ObjectId,
+            ref: 'Doctor',
+        },
+        patientId: {
+            type: mongoose.Types.ObjectId,
+            ref: 'Patient',
+        },
+        date: {
+            type: String,
+            required: [true, 'Appointment date is required!'],
+        },
+        time: {
+            type: String,
+            required: [true, 'Appointment hour is required!'],
+        },
+        reason: {
+            type: String,
+            required: [true, 'Reason for appointment is required!'],
+            minLength: [5, 'Reason must be at least 5 characters!'],
+            maxLength: [50, 'Reason must not be more than 50 characters!'],
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Appointment = mongoose.model('Appointment', appointmentSchema);
+
+module.exports = Appointment;
