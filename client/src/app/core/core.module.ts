57ff2cc1d2b0ed5b934d568d99e186a768bb6858
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-03-22 14:28:24 +0200

    Add header and footer templates

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -8,7 +8,7 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     CommonModule
