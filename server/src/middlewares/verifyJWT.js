d32a4c8c7c2963c974934c9111cba74ed32f161b
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-03-30 15:24:50 +0200

    Update verifyJWT.js

--- server/src/middlewares/verifyJWT.js
+++ server/src/middlewares/verifyJWT.js
@@ -1,27 +1,34 @@
 const jwt = require('../lib/jwt');
 
-const verifyJWT = (req, res, next) => {
+const verifyJWT = async (req, res, next) => {
     const authHeader = req.headers.authorization || req.headers.Authorization;
 
     if (!authHeader?.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'Unauthorized!' });
+        delete req.headers['Authorization'] || delete req.headers['authorization'];
+        console.log('Token is missing!')
+        return res.status(401).json({ message: 'Token is missing!' });
     }
 
     const token = authHeader.split(' ')[1];
 
-    jwt.verify(
-        token,
-        process.env.ACCESS_TOKEN_SECRET,
-        (err, decoded) => {
-            if (err) {
-                return res.status(403).json({ message: 'Forbidden!' });
-            }
+    try {
+        const decoded = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-            req.user = decoded.UserInfo;
+        req.user = decoded.UserInfo; // req.user = { _id, username, email }
 
-            next();
+        next();
+
+    } catch (error) {
+        delete req.headers['Authorization'] || delete req.headers['authorization'];
+
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ message: 'Token has expired!' });
+        } else {
+            console.log(error.name)
+            console.log('Problem with token verification!')
+            return res.status(403).json({ message: 'Problem with token verification!' });
         }
-    )
+    }
 };
 
 module.exports = verifyJWT;
