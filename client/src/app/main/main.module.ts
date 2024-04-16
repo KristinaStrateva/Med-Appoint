c6c6da2a11dae6c9904494bd2da6d0bcc23715f7
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-04-16 23:19:13 +0300

    Add Home Page component

--- client/src/app/main/main.module.ts
+++ client/src/app/main/main.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    HomePageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
   ],
   exports: [
-    MainComponent
+    MainComponent,
+    HomePageComponent,
   ],
 })
 export class MainModule { }
