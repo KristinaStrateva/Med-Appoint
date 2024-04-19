08ff4616216613ba0101c636854c86c7405b7ace
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-04-19 15:45:40 +0300

    Add main routes and lazy loading

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './main/home-page/home-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', component: HomePageComponent },
+  {
+    path: 'auth',
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+  },
+  {
+    path: 'site',
+    loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
+  },
+  { path: '**', redirectTo: '/404' },
+  { path: '404', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
