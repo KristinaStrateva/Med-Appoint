017273df08eb340e54933bc13acddae903617d4f
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-04-19 15:44:38 +0300

    Add Not Found Page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { MainModule } from './main/main.module';
-import { UserModule } from './user/user.module';
+// import { UserModule } from './user/user.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     CoreModule,
     SharedModule,
     MainModule,
-    UserModule,
+    // UserModule,
+    HttpClientModule,
     AppRoutingModule,
   ],
   providers: [],
