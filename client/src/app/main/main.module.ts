0ac0187747ad20e97346ec42c2d52fda9971dddf
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-04-19 20:53:24 +0300

    Update main.module.ts

--- client/src/app/main/main.module.ts
+++ client/src/app/main/main.module.ts
@@ -3,17 +3,25 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SharedModule } from '../shared/shared.module';
+import { AboutComponent } from './about/about.component';
+import { MainRoutingModule } from './main-routing.module';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
     MainComponent,
-    HomePageComponent
+    HomePageComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
+    MainRoutingModule,
+    RouterModule
   ],
   exports: [
     MainComponent,
