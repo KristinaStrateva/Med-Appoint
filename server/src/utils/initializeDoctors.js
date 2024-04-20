42e10fde54bc7bd3a12a97eab35a3b25523e5f1a
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-04-20 15:19:59 +0300

    Update initializeDoctors.js

--- server/src/utils/initializeDoctors.js
+++ server/src/utils/initializeDoctors.js
@@ -7,12 +7,20 @@ const initializeDoctors = asyncHandler(async () => {
 
     if (existingDoctors.length === 0) {
         await Doctor.create([
-            { name: 'Dr. Peter Davidson', medSpeciality: 'Neurology' },
-            { name: 'Dr. Paola Dhornan', medSpeciality: 'Pediatrics' },
             { name: 'Dr. Louise Parker', medSpeciality: 'General Practice/GP' },
+            { name: 'Dr. Megan Burnet', medSpeciality: 'General Practice/GP' },
+            { name: 'Dr. Peter Strong', medSpeciality: 'General Practice/GP' },
+            { name: 'Dr. Paola Dhornan', medSpeciality: 'Pediatrics' },
+            { name: 'Dr. Linda Murray', medSpeciality: 'Pediatrics' },
             { name: 'Dr. George Brown', medSpeciality: 'Orthopedics' },
-            { name: 'Dr. Bethany Rose', medSpeciality: 'Dermatology' },
+            { name: 'Dr. Peter Davidson', medSpeciality: 'Neurology' },
+            { name: 'Dr. Sarah Silverstone', medSpeciality: 'Neurology' },
             { name: 'Dr. David Noah', medSpeciality: 'Cardiology' },
+            { name: 'Dr. Bill James', medSpeciality: 'Cardiology' },
+            { name: 'Dr. Bethany Rose', medSpeciality: 'Dermatology' },
+            { name: 'Dr. Linsey Williams', medSpeciality: 'Dermatology' },
+            { name: 'Dr. Bob Grey', medSpeciality: 'Dermatology' },
+            { name: 'Dr. Mariah Susane', medSpeciality: 'Dermatology' },
         ]);
     }
 });
