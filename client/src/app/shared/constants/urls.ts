c11c96b588461bfc9ce7dfa946f153f92dd11173
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-04-21 23:52:06 +0300

    Extract pacient urls for requests as constants

---
+++ client/src/app/shared/constants/urls.ts
@@ -0,0 +1,8 @@
+const BASE_URL = 'http://localhost:3500';
+
+// export const DOCTORS_URL = BASE_URL + '/doctors';
+
+
+export const PATIENT_LOGIN_URL = BASE_URL + '/patients/login';
+export const PATIENT_REGISTER_URL = BASE_URL + '/patients/register';
+export const PATIENT_LOGOUT_URL = BASE_URL + '/patients/logout';
