55fce36b2561a68bb5f425100e228e6a5d94d556
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-04-24 23:46:27 +0300

    Update header component to know if the user is logged in or not

--- client/src/app/core/header/header.component.ts
+++ client/src/app/core/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Patient } from 'src/app/shared/models/Patient';
+import { PatientService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  patient!: Patient;
 
+  constructor(private patientService: PatientService) {
+    patientService.patient$.subscribe(currPatient => {
+      this.patient = currPatient;
+    })
+  }
+
+  logoutHandler(): void { }
 }
