3f9b2ba0fadef05bf7e3d0f60f7eb7e8f0ef0297
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-04-27 01:16:31 +0300

    Update response for home page
    return { allDoctors, medSepcialities } instead of only allDoctors

--- server/src/controllers/homeController.ts
+++ server/src/controllers/homeController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
-import Doctor from '../models/Doctor';
+import Doctor, { DoctorDocument } from '../models/Doctor';
 
 // @desc Get all doctors
 // @route GET /
@@ -12,7 +12,17 @@ const getAllDoctors = asyncHandler(async (req: Request, res: Response) => {
         .populate('appointments')
         .lean();
 
-    res.json(allDoctors);
+    const medSpecialities: DoctorDocument[] = await Doctor.aggregate([
+        { $group: { _id: '$medSpeciality', doctor: { $first: '$$ROOT' } } },
+        { $replaceRoot: { newRoot: '$doctor' } }
+    ])
+
+    res.json(
+        {
+            allDoctors,
+            medSpecialities
+        }
+    );
 });
 
 export default { getAllDoctors };
