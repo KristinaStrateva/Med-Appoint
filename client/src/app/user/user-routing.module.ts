c73c8938f1326eb1bb97126f1477c0889b5b841e
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-05-13 15:53:26 +0300

    Add canActivate when auth guard is true for profile page

--- client/src/app/user/user-routing.module.ts
+++ client/src/app/user/user-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { ProfileComponent } from "./profile/profile.component";
+import { AuthGuard } from "../auth/guards/auth.guard";
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'profile', component: ProfileComponent },
+    { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
