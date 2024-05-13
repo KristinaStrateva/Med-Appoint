c12294086f0dd1580718773c22908b8a367b4b1b
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-05-14 01:28:40 +0300

    Update core.module.ts

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
