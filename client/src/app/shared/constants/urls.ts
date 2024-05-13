82c6bcb5d315af54457da47a98e7b53593a7ba3a
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-05-14 01:29:54 +0300

    Update urls.ts

--- client/src/app/shared/constants/urls.ts
+++ client/src/app/shared/constants/urls.ts
@@ -2,7 +2,5 @@ const BASE_URL = 'http://localhost:3500';
 
 export const DOCTORS_URL = BASE_URL + '/';
 
-
 export const PATIENT_LOGIN_URL = BASE_URL + '/patients/login';
 export const PATIENT_REGISTER_URL = BASE_URL + '/patients/register';
-export const PATIENT_LOGOUT_URL = BASE_URL + '/patients/logout';
