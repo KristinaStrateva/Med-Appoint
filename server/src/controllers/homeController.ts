7bc1edb1b842c408ed4252db214c9594f2f22109
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-05-15 18:48:15 +0300

    Return patients amount from server

--- server/src/controllers/homeController.ts
+++ server/src/controllers/homeController.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
 import Doctor, { DoctorDocument } from '../models/Doctor';
+import Patient from '../models/Patient';
 
 // @desc Get all doctors
 // @route GET /
 // @access Public
 
 const getAllDoctors = asyncHandler(async (req: Request, res: Response) => {
-    const allDoctors = await Doctor
+    const allDoctors: DoctorDocument = await Doctor
         .find()
         .populate('appointments')
         .lean();
@@ -15,12 +16,15 @@ const getAllDoctors = asyncHandler(async (req: Request, res: Response) => {
     const medSpecialities: DoctorDocument[] = await Doctor.aggregate([
         { $group: { _id: '$medSpeciality', doctor: { $first: '$$ROOT' } } },
         { $replaceRoot: { newRoot: '$doctor' } }
-    ])
+    ]);
+
+    const patientsAmount = await Patient.countDocuments();
 
     res.json(
         {
             allDoctors,
-            medSpecialities
+            medSpecialities,
+            patientsAmount
         }
     );
 });
