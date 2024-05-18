e86bdae05a36a4f1fbcb9d5a573900a9b80a1dc4
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-05-18 03:35:59 +0300

    Add generate hours util function

---
+++ client/src/app/shared/utils/generateHours.ts
@@ -0,0 +1,13 @@
+const generateHours: string[] = [
+    '09:00',
+    '10:00',
+    '11:00',
+    '12:00',
+    '13:00',
+    '14:00',
+    '15:00',
+    '16:00',
+    '17:00',
+];
+
+export default generateHours;
